8aac6d1e1c8dd0e454ed15fcd05b50a11ddd0692
신광웅 <shingw@gccorp.com>
2023-06-08 08:45:09 +0900

    order jooq

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -1,9 +1,14 @@
+import nu.studer.gradle.jooq.JooqEdition
+import org.jooq.meta.jaxb.Logging
+import org.jooq.meta.jaxb.SchemaMappingType
+
 plugins {
     kotlin("jvm")
     kotlin("kapt")
     id("org.springframework.boot") version "3.0.3"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jetbrains.kotlin.plugin.spring") version "1.8.10"
+    id("nu.studer.jooq") version "8.1"
 }
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.targetCompatibility = JavaVersion.VERSION_17
@@ -13,6 +18,9 @@ dependencies {
     implementation(libs.bundles.r2dbc.postgres)
     implementation(libs.bundles.r2dbc.querydsl)
     kapt(libs.bundles.r2dbc.querydsl)
+    implementation("org.jooq:jooq:3.18.2")
+    implementation("org.jooq:jooq-codegen:3.18.2")
+    implementation("org.jooq:jooq-meta:3.18.2")
     implementation(libs.spring.gateway)
 }
 configurations { all { exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging") } }
@@ -29,5 +37,43 @@ tasks {
     }
     getByName<Jar>("jar") {
         enabled = false
+    }
+}
+jooq {
+    version.set("3.18.2")
+    edition.set(JooqEdition.OSS)
+    configurations{
+        create("main") {
+            jooqConfiguration.apply {
+                logging = Logging.WARN
+                jdbc.apply {
+                    driver = "org.postgresql.Driver"
+                    url = "jdbc:postgresql://libra:5432/report_service"
+                    user = "report_service"
+                    password = "rs1004!@"
+                }
+                generator.apply {
+                    name = "org.jooq.codegen.KotlinGenerator"
+                    database.apply {
+                        name = "org.jooq.meta.postgres.PostgresDatabase"
+                        schemata = listOf(
+                            SchemaMappingType().withInputSchema("rms"),
+                        )
+                    }
+                    generate.apply {
+                        isDeprecated = false
+                        isRecords = true
+                        isPojos = true
+                        isJpaAnnotations = false
+                        isFluentSetters = false
+                    }
+                    target.apply {
+                        packageName = "com.gcgenome.lims"
+                        directory = "build/generated/jooq"
+                    }
+                    strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+                }
+            }
+        }
     }
 }
