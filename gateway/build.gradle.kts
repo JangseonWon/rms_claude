640948c532dcc531a52362657b4df6cdf9b3b0a5
xodyd <xodyd@greencross.com>
2023-07-03 17:33:32 +0900

    k8s test

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,34 +1,21 @@
 plugins {
     kotlin("jvm")
-    kotlin("kapt")
     id("org.springframework.boot") version "3.0.3"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.8.10"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.8.22"
 }
-java.sourceCompatibility = JavaVersion.VERSION_17
-java.targetCompatibility = JavaVersion.VERSION_17
 dependencies {
     implementation(libs.bundles.spring.client)
     implementation(libs.bundles.kotlin.webflux)
     implementation(libs.bundles.r2dbc.postgres)
     implementation(libs.bundles.r2dbc.querydsl)
     implementation(libs.bundles.jjwt.runtime)
-    kapt(libs.bundles.r2dbc.querydsl)
     implementation(libs.spring.gateway)
 }
 configurations { all { exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging") } }
 dependencyManagement { imports { mavenBom(libs.spring.cloud.bom.get().toString()) } }
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-    }
-    processResources {
-        if(project.gradle.startParameter.taskNames.contains("build")) exclude("application.yml")
-    }
-    getByName<Jar>("jar") {
-        enabled = false
-    }
+kotlin.jvmToolchain(17)
+tasks.processResources {
+    if(project.gradle.startParameter.taskNames.contains("build")) exclude("application.yml")
 }
+springBoot.mainClass.set("com.gcgenome.rms.ApplicationKt")
