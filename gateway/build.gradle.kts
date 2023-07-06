298f905b5059baad905e4e21401768a4416dce04
xodyd <xodyd@greencross.com>
2023-07-06 09:46:17 +0900

    gateway: gradle task name 확인

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -1,13 +1,10 @@
 plugins {
     kotlin("jvm")
-    id("java")
     id("org.springframework.boot") version "3.0.3"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.jetbrains.kotlin.plugin.spring") version "1.8.22"
     id("com.google.cloud.tools.jib") version "3.3.2"
 }
-java.sourceCompatibility = JavaVersion.VERSION_17
-java.targetCompatibility = JavaVersion.VERSION_17
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -29,9 +26,6 @@ jib {
     from {
         image = "eclipse-temurin:17.0.7_7-jre-jammy"
     }
-    to {
-        image = "image-registry.apps.lims.gcgenome.com/rms-test/gateway"
-    }
     container {
         environment = mapOf(
             "LANG" to "C.UTF-8",
@@ -42,16 +36,8 @@ jib {
 
 configurations { all { exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging") } }
 dependencyManagement { imports { mavenBom(libs.spring.cloud.bom.get().toString()) } }
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-    }
-    processResources {
+kotlin.jvmToolchain(17)
+tasks.processResources {
+        println("gradle project task name: ${project.gradle.startParameter.taskNames}")
         if(project.gradle.startParameter.taskNames.contains("build")) exclude("application.yml")
-    }
-    getByName<Jar>("jar") {
-        enabled = false
-    }
 }
