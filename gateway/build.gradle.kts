649a5dda11ad4526c1d0bf09bc767f5211afeb41
xodyd <xodyd@greencross.com>
2023-09-13 20:25:34 +0900

    .

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -37,6 +37,7 @@ jib {
     }
     to{
         image = "image-registry.openshift-image-registry.svc:5000/rms-test/rms-gateway"
+        tags = setOf(System.getenv("REGISTRY_TAG"), "latest")
         auth {
             username = System.getenv("REGISTRY_USERNAME")
             password = System.getenv("REGISTRY_PASSWORD")
