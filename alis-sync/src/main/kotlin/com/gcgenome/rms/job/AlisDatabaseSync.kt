425c040353953f96ca2584c3337e42bdc61e2202
신광웅 <shingw@gccorp.com>
2023-10-24 09:29:55 +0900

    alis-sync module: 잡 분리

---
+++ alis-sync/src/main/kotlin/com/gcgenome/rms/job/AlisDatabaseSync.kt
@@ -0,0 +1,43 @@
+package com.gcgenome.rms.job
+
+import com.gcgenome.rms.dao.*
+import com.gcgenome.rms.data.RmsOrder
+import com.gcgenome.rms.data.Sample
+import org.jooq.DSLContext
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+
+@Service
+class AlisDatabaseSync (
+    private val dslContext: DSLContext
+): AlisDao, OrderDao, ItemDao, SampleDao, PatientDao, OrganizationDao, ExtensionDao, UserDao {
+    private val logger = LoggerFactory.getLogger("rms sync")
+
+    fun alisDatabaseSync(page: Int, limit: Int, fromDate: String, toDate: String): Mono<Sample> {
+        logger.info("fun start")
+        return Mono.from(dslContext.transactionPublisher{ trx ->
+            trx.dsl().run {
+                logger.info("alis query start")
+                selectAlisOrder(page - 1, limit, fromDate, toDate).flatMap { alisOrder ->
+                    val rmsOrder = RmsOrder.toModel(alisOrder)
+                    checkSampleByServiceId(rmsOrder.genomeBarcode, rmsOrder.serviceId)
+                        .switchIfEmpty(insertUser(rmsOrder)
+                            .then(insertOrganization(rmsOrder))
+                            .then(insertPatient(rmsOrder))
+                            .then(insertOrder(rmsOrder))
+                            .then(insertItem(rmsOrder))
+                            .then(insertSample(rmsOrder))
+                            .then(selectSampleExtension(rmsOrder.createAt, rmsOrder.orderNumber)
+                                .flatMap {  sampleExtension ->
+                                    insertSampleExtension(sampleExtension, rmsOrder.sampleId)
+                                }.toMono()
+                            ).then(checkSampleByServiceId(rmsOrder.genomeBarcode, rmsOrder.serviceId))
+                        )
+                }
+            }
+        })
+    }
+}
--- alis-sync/src/main/kotlin/com/gcgenome/rms/job/ScheduleService.kt
+++ alis-sync/src/main/kotlin/com/gcgenome/rms/job/ScheduleService.kt
@@ -1,22 +1,20 @@
 package com.gcgenome.rms.job
 
 import com.gcgenome.rms.dao.*
-import com.gcgenome.rms.data.RmsOrder
-import com.gcgenome.rms.data.Sample
 import org.jooq.DSLContext
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
-import kotlin.time.Duration
 
 @Service
 class ScheduleService (
-    val dslContext: DSLContext
-): AlisDao, OrderDao, ItemDao, SampleDao, PatientDao, OrganizationDao, ExtensionDao, UserDao{
+    val dslContext: DSLContext,
+    val alisDatabaseSync: AlisDatabaseSync
+): AlisDao {
     private val logger = LoggerFactory.getLogger("rms sync")
+
 
     @Scheduled(fixedDelay = 1000L*60*60)
     fun executeScheduledTask() {
@@ -28,7 +26,7 @@ class ScheduleService (
 
     fun dataSyncBatch(): Mono<Void> {
         val fromDate = "2023-01-01T00:00:00"
-        val toDate = "2023-01-07T23:00:00"
+        val toDate = "2023-01-31T23:00:00"
         val limit = 20
 
         return dslContext.selectAlisOrderCount(fromDate, toDate)
@@ -36,35 +34,10 @@ class ScheduleService (
                 logger.info("count : $count")
                 val batchCount = count / limit + if (count % limit == 0) 0 else 1
                 Flux.range(1, batchCount)
-                    .flatMap { i -> alisDatabaseSync(i, limit, fromDate, toDate)
+                    .flatMap { i -> alisDatabaseSync.alisDatabaseSync(i, limit, fromDate, toDate)
                         .doOnSuccess { logger.info("TOTAL COUNT : $count / BATCH COUNT : $batchCount / CURRENT COUNT : $i") } }
                     .then()
             }
-    }
-
-    fun alisDatabaseSync(page: Int, limit: Int, fromDate: String, toDate: String): Mono<Sample> {
-        logger.info("fun start")
-        return Mono.from(dslContext.transactionPublisher{ trx ->
-            trx.dsl().run {
-                logger.info("alis query start")
-                selectAlisOrder(page - 1, limit, fromDate, toDate).flatMap { alisOrder ->
-                    val rmsOrder = RmsOrder.toModel(alisOrder)
-                    checkSampleByServiceId(rmsOrder.genomeBarcode, rmsOrder.serviceId)
-                        .switchIfEmpty(insertUser(rmsOrder)
-                            .then(insertOrganization(rmsOrder))
-                            .then(insertPatient(rmsOrder))
-                            .then(insertOrder(rmsOrder))
-                            .then(insertItem(rmsOrder))
-                            .then(insertSample(rmsOrder))
-                            .then(selectSampleExtension(rmsOrder.createAt, rmsOrder.orderNumber)
-                                .flatMap {  sampleExtension ->
-                                    insertSampleExtension(sampleExtension, rmsOrder.sampleId)
-                                }.toMono()
-                            ).then(checkSampleByServiceId(rmsOrder.genomeBarcode, rmsOrder.serviceId))
-                        )
-                }
-            }
-        })
     }
 
 }
