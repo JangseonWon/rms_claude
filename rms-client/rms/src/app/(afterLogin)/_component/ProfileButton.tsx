b9781da08a03fa49388a2448d82ff26e92020727
ShinGwangWoong <shingw@gcgenome.com>
2024-06-07 10:39:17 +0900

    client: 사용자정보 클릭 수정

--- rms-client/rms/src/app/(afterLogin)/_component/ProfileButton.tsx
+++ rms-client/rms/src/app/(afterLogin)/_component/ProfileButton.tsx
@@ -2,7 +2,7 @@
 
 import style from "./profileButton.module.css"
 import {Session} from "@auth/core/types";
-import React, {useEffect, useRef, useState} from "react";
+import React, {useRef, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faUser} from "@fortawesome/free-regular-svg-icons";
 import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
@@ -17,7 +17,6 @@ export default function ProfileButton({session}: Props) {
     const [showDropdown, setShowDropdown] = useState(false);
     const router = useRouter()
     const dropdownRef = useRef<HTMLDivElement>(null);
-    const timerRef = useRef<NodeJS.Timeout | null>(null);
 
     const onLogout = () =>{
         signOut({redirect: false})
@@ -34,37 +33,15 @@ export default function ProfileButton({session}: Props) {
     }
 
     const handleMouseEnter = () => {
-        if (timerRef.current) {
-            clearTimeout(timerRef.current);
-        }
         setShowDropdown(true);
     };
 
     const handleMouseLeave = () => {
-        timerRef.current = setTimeout(() => {
-            setShowDropdown(false);
-        }, 300);
+        setShowDropdown(false);
     };
-
-    const handleClickOutside = (event: MouseEvent) => {
-        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-            setShowDropdown(false);
-        }
-    };
-
-    useEffect(() => {
-        if (showDropdown) {
-            document.addEventListener("mousedown", handleClickOutside);
-        } else {
-            document.removeEventListener("mousedown", handleClickOutside);
-        }
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [showDropdown]);
 
     return(
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div className={style.profileContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className={style.profile}>
                 <FontAwesomeIcon icon={faUser} />
                 <span>{session?.user?.name}</span>
