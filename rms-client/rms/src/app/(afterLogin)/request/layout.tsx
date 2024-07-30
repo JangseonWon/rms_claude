630a4068b510f69e17878fc166762036a655caa3
ShinGwangWoong <shingw@gcgenome.com>
2024-07-30 16:22:29 +0900

    client/order-service: 테이블,home 화면 수정

--- rms-client/rms/src/app/(afterLogin)/request/layout.tsx
+++ rms-client/rms/src/app/(afterLogin)/request/layout.tsx
@@ -4,7 +4,7 @@ import {ReactNode} from "react";
 import NavMenu from "@/app/(afterLogin)/request/_component/NavMenu";
 import style from "@/app/(afterLogin)/request/layout.module.css"
 import AlertDialog from "@/app/_component/AlertDialog";
-import {useAlertDialog, useIconAlertDialog, useMessageAlertDialog} from "@/store/useAlertDialogStore";
+import {useAlertDialog, useMessageAlertDialog} from "@/store/useAlertDialogStore";
 import NoticeDialog from "@/app/_component/NoticeDialog";
 import {useMessageNoticeDialog, useNoticeDialog} from "@/store/useNoticeDialogStore";
 
@@ -12,13 +12,12 @@ type Props = { children: ReactNode, modal: ReactNode }
 export default function Layout({ children, modal }: Props) {
     const showAlertDialog = useAlertDialog();
     const alertMessage = useMessageAlertDialog();
-    const icon = useIconAlertDialog();
     const showNoticeDialog = useNoticeDialog();
     const noticeMessage = useMessageNoticeDialog();
 
     return (
         <div className={style.container}>
-            {showAlertDialog && (<AlertDialog icon={icon} message={alertMessage}/>)}
+            {showAlertDialog && (<AlertDialog message={alertMessage}/>)}
             {showNoticeDialog && (<NoticeDialog message={noticeMessage}/>)}
             <div className={style.leftSection}>
                 <NavMenu/>
--- rms-client/rms/src/app/(beforeLogin)/layout.tsx
+++ rms-client/rms/src/app/(beforeLogin)/layout.tsx
@@ -5,17 +5,16 @@ import style from "@/app/(beforeLogin)/layout.module.css";
 import Header from "@/app/(beforeLogin)/_component/Header";
 import Footer from "@/app/_component/Footer";
 import AlertDialog from "@/app/_component/AlertDialog";
-import {useAlertDialog, useIconAlertDialog, useMessageAlertDialog} from "@/store/useAlertDialogStore";
+import {useAlertDialog, useMessageAlertDialog} from "@/store/useAlertDialogStore";
 
 type Props = { children: ReactNode};
 export default function Layout({ children }: Props) {
     const showAlertDialog = useAlertDialog();
     const message = useMessageAlertDialog();
-    const icon = useIconAlertDialog();
 
     return (
         <div className={style.container}>
-            {showAlertDialog && (<AlertDialog icon={icon} message={message}/>)}
+            {showAlertDialog && (<AlertDialog message={message}/>)}
             <section className={style.topSection}>
                 <Header/>
             </section>
