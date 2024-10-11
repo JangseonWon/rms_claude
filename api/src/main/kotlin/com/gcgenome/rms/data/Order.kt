2238296e1c0090ee4bc596748f232a33c7761f93
xodyd <xodyd@greencross.com>
2024-10-11 16:38:35 +0900

    api: a-lis api 제공(2)

--- api/src/main/kotlin/com/gcgenome/rms/data/Order.kt
+++ api/src/main/kotlin/com/gcgenome/rms/data/Order.kt
@@ -1,24 +1,5 @@
 package com.gcgenome.rms.data
 
-import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
-import com.fasterxml.jackson.databind.annotation.JsonSerialize
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
-import java.time.LocalDateTime
-import java.util.*
-
 data class Order(
-    @JsonProperty("id")
-    val id: UUID?,
-    @JsonProperty("user_id")
-    val userId: String?,
-    @JsonProperty("serial")
-    val serial: String?,
-    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
-    @JsonSerialize(using = LocalDateTimeSerializer::class)
-    @JsonProperty("create_at")
-    val createAt: LocalDateTime?,
-    @JsonProperty("requests")
-    val requests: Array<Request>?
+    val test: String
 )
