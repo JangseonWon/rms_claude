12dd3b185ab4824b899f858754b5116d07855b2d
ShinGwangWoong <shingw@gcgenome.com>
2024-10-22 16:50:15 +0900

    product-service: 오류 수정

--- product-service/src/main/kotlin/com/gcgenome/rms/product/service/ServiceHandler.kt
+++ product-service/src/main/kotlin/com/gcgenome/rms/product/service/ServiceHandler.kt
@@ -3,9 +3,9 @@ package com.gcgenome.rms.product.service
 import com.gcgenome.rms.authentication.UserAuthentication
 import com.gcgenome.rms.dao.*
 import com.gcgenome.rms.data.Service
+import com.gcgenome.rms.tables.references.SERVICE
 import com.gcgenome.rms.tables.references.USER_SERVICE
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import java.util.*
@@ -15,7 +15,7 @@ class ServiceHandler(val dslContext: DSLContext):
     OrganizationDao, ServiceDao, SampleTypeDao, OrderDao, RequestDao, PatientDao, SampleDao, SampleExtensionDao
 {
     fun getServices(user: UserAuthentication, categoryId: UUID): Flux<Service> {
-        val andWhere = if (user.user.role == "USER") USER_SERVICE.USER_ID.eq(user.user.id) else DSL.noCondition()
+        val andWhere = if (user.user.role == "USER") USER_SERVICE.USER_ID.eq(user.user.id) else SERVICE.CATEGORY_ID.eq(categoryId)
         return dslContext.selectServiceByUserIdAndCategoryId(andWhere, categoryId)
     }
 }
