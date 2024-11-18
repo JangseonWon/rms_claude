47344aafbf3a01007f03f68aefb2d2f3da0e9043
ShinGwangWoong <shingw@gcgenome.com>
2024-11-18 16:44:52 +0900

    entity,order-service: post id 수정

--- entity/src/main/kotlin/com/gcgenome/rms/entity/PostFile.kt
+++ entity/src/main/kotlin/com/gcgenome/rms/entity/PostFile.kt
@@ -2,14 +2,13 @@ package com.gcgenome.rms.entity
 
 import jakarta.persistence.*
 import java.time.LocalDateTime
-import java.util.UUID
 
 @Entity
 @Table(schema = "rms_dev", name = "post_file")
 data class PostFile(
     @Id
     @Column(name = "id")
-    val id: UUID,
+    val id: Long,
     @Column(name = "path", nullable = false)
     val path: String,
     @Column(name = "name", length = 64, nullable = false)
