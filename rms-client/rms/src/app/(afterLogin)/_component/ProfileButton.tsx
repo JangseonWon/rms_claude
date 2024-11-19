e402b07fa472c143c8f048ef13bc876ab9cca1a5
ShinGwangWoong <shingw@gcgenome.com>
2024-11-19 15:31:55 +0900

    client: alarm 화면 추가

--- rms-client/rms/src/app/(afterLogin)/_component/ProfileButton.tsx
+++ rms-client/rms/src/app/(afterLogin)/_component/ProfileButton.tsx
@@ -7,10 +7,12 @@ import {faBell, faUser} from "@fortawesome/free-regular-svg-icons";
 import {faChevronDown} from "@fortawesome/free-solid-svg-icons";
 import {useRouter} from "next/navigation";
 import {signOut, useSession} from "next-auth/react";
+import ProfileAlarm from "@/app/(afterLogin)/_component/ProfileAlarm";
 
 export default function ProfileButton() {
     const { data: session } = useSession();
-    const [showDropdown, setShowDropdown] = useState(false);
+    const [profileOpen, setProfileOpen] = useState(false);
+    const [alarmOpen, setAlarmOpen] = useState(false);
     const router = useRouter()
 
     const onLogout = () =>{
@@ -23,20 +25,29 @@ export default function ProfileButton() {
         router.push('/user')
     }
 
-    const handleMouseEnter = () => {
-        setShowDropdown(true);
+    const handleAlarmToggle = () => {
+        setAlarmOpen(!alarmOpen);
     };
 
-    const handleMouseLeave = () => {
-        setShowDropdown(false);
+    const handleUserMouseEnter = () => {
+        setProfileOpen(true);
+    };
+
+    const handleUserMouseLeave = () => {
+        setProfileOpen(false);
     };
 
     return(
         <div className={style.container}>
-            <div className={style.profile}>
-                <FontAwesomeIcon className={style.bell} icon={faBell}/>
+            <div className={style.profile} onClick={handleAlarmToggle}>
+                <FontAwesomeIcon className={`${style.bell} ${alarmOpen ? style.bellActive : ''}`} icon={faBell}/>
             </div>
-            <div className={style.profileContainer} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+            {alarmOpen && (
+                <div className={`${style.profileAlarm} active`}>
+                    <ProfileAlarm />
+                </div>
+            )}
+            <div className={style.profileContainer} onMouseEnter={handleUserMouseEnter} onMouseLeave={handleUserMouseLeave}>
                 <div className={style.profile}>
                     <FontAwesomeIcon className={style.user} icon={faUser}/>
                     <div className={style.userName}>
@@ -44,8 +55,8 @@ export default function ProfileButton() {
                     </div>
                     <FontAwesomeIcon className={style.ChevronDown} icon={faChevronDown}/>
                 </div>
-                {showDropdown && (
-                    <div className={style.profileDropdown} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                {profileOpen && (
+                    <div className={style.profileDropdown} onMouseEnter={handleUserMouseEnter} onMouseLeave={handleUserMouseLeave}>
                         <ul>
                             <li onClick={onProfile}>My profile</li>
                             <li onClick={onLogout}>Logout</li>
