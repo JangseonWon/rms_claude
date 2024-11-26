d245d4fb677dfb5d7a7a990ea83df5c298ffc7ff
xodyd <xodyd@greencross.com>
2024-11-26 18:43:10 +0900

    cart-service: ORDER테이블 수정 반영

--- cart-service/src/main/kotlin/com/gcgenome/rms/dao/OrderDao.kt
+++ cart-service/src/main/kotlin/com/gcgenome/rms/dao/OrderDao.kt
@@ -4,11 +4,7 @@ import com.gcgenome.rms.data.Order
 import com.gcgenome.rms.tables.references.ORDER
 import com.gcgenome.rms.tables.references.REQUEST
 import org.jooq.DSLContext
-import org.jooq.impl.DSL.*
 import reactor.core.publisher.Mono
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 
 interface OrderDao {
@@ -20,25 +16,6 @@ interface OrderDao {
                         selectOne().from(REQUEST).where(REQUEST.ORDER_ID.eq(orderId))
                     )
                 ).returning()
-        ).map { it.into(Order::class.java) }
-    }
-    fun DSLContext.updateOrderSerialAndCreatedAtById(orderId: UUID, userId: String): Mono<Order> {
-        val ofPattern = DateTimeFormatter.ofPattern("yyyyMMdd")
-        val currentDate = LocalDate.now().format(ofPattern)
-        val serialPrefix = userId + currentDate
-        val defaultSerial = serialPrefix + "0001"
-
-        return Mono.from(
-            update(ORDER)
-                .set(ORDER.CREATE_AT, LocalDateTime.now())
-                .set(ORDER.SERIAL,
-                        select(coalesce(
-                            concat(inline(serialPrefix), lpad((right(max(ORDER.SERIAL), 4).cast(Int::class.java).plus(1)).cast(String::class.java), 4, '0'))
-                            , defaultSerial
-                        )).from(ORDER).where(ORDER.SERIAL.like("$serialPrefix%"))
-                )
-                .where(ORDER.ID.eq(orderId))
-                .returning()
         ).map { it.into(Order::class.java) }
     }
 }
