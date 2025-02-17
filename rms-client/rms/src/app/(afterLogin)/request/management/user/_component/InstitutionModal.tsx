1c99d466d2794b156b4b95620264f7d731b7fa40
ShinGwangWoong <shingw@gcgenome.com>
2025-02-17 15:36:36 +0900

    client: modal css 수정

--- rms-client/rms/src/app/(afterLogin)/request/management/user/_component/InstitutionModal.tsx
+++ rms-client/rms/src/app/(afterLogin)/request/management/user/_component/InstitutionModal.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from "react";
-// import style from "@/app/(afterLogin)/request/management/user/_component/institutionModal.module.css";
+import scrollbar from '@/css/scrollBar.module.css';
 import style from '@/css/modal.module.css';
 import tableStyle from '@/css/globalTable.module.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faXmark} from "@fortawesome/free-solid-svg-icons";
 import {Organization} from "@/model/Organization";
 import {getInstitutions} from "@/app/(afterLogin)/request/management/user/_api/getInstitutions";
+import classNames from "classnames";
 
 type Props = {
     id: string;
@@ -45,32 +46,34 @@ export default function InstitutionModal({id, name, open, closeModal}: Props) {
                 <div className={style.modalSubTitle}>
                     User Id : {id} / Name : {name}
                 </div>
-                <section className={style.modalBody}>
-                    <div className={style.leftBody}>
-                        <table className={tableStyle.table}>
-                            <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Code</th>
-                                <th>Nursing Number</th>
-                                <th>Type</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {institutionData && institutionData.length > 0 && institutionData.map((row, rowIndex) => (
-                                <tr key={rowIndex}>
-                                    <td>{row.id}</td>
-                                    <td>{row.name}</td>
-                                    <td>{row.registration_number}</td>
-                                    <td>{row.nursing_number}</td>
-                                    <td>{row.type}</td>
+                <div className={classNames(style.wrapper, scrollbar.default)}>
+                    <section className={style.modalBody}>
+                        <div className={style.leftBody}>
+                            <table className={tableStyle.table}>
+                                <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Name</th>
+                                    <th>Code</th>
+                                    <th>Nursing Number</th>
+                                    <th>Type</th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
+                                </thead>
+                                <tbody>
+                                {institutionData && institutionData.length > 0 && institutionData.map((row, rowIndex) => (
+                                    <tr key={rowIndex}>
+                                        <td>{row.id}</td>
+                                        <td>{row.name}</td>
+                                        <td>{row.registration_number}</td>
+                                        <td>{row.nursing_number}</td>
+                                        <td>{row.type}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </section>
+                </div>
             </div>
         </div>
     )
