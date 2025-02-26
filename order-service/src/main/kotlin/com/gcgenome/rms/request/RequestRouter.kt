d2d41dce224bb8c929e4ae155c00a407c3bfcfc9
xodyd <xodyd@greencross.com>
2025-02-26 10:51:06 +0900

    order-service: 에러로그 추가

--- order-service/src/main/kotlin/com/gcgenome/rms/request/RequestRouter.kt
+++ order-service/src/main/kotlin/com/gcgenome/rms/request/RequestRouter.kt
@@ -5,6 +5,8 @@ import com.gcgenome.rms.data.Query
 import com.gcgenome.rms.data.RequestDTO
 import com.gcgenome.rms.exception.AuthenticationNotFoundException
 import com.gcgenome.rms.exception.OrderNotFoundException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
@@ -26,6 +28,7 @@ class RequestRouter(
         PATCH("/w-api/order-service/requests",:: updateRequests)
         GET("/w-api/order-service/services/{service_id}/samples/{sample_id}", ::orderInfo)
     }
+    private val logger: Logger = LoggerFactory.getLogger(RequestRouter::class.java)
     private fun selectRequests(request: ServerRequest) : Mono<ServerResponse> {
         return authenticationHandler.principal(request).zipWith(request.bodyToMono(Query::class.java))
             .flatMap { requestHandler.selectRequests(it.t1, it.t2) }
@@ -37,7 +40,10 @@ class RequestRouter(
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(it.data), RequestDTO::class.java ) }
             .onErrorResume(AuthenticationNotFoundException::class.java) { e -> ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("${e.message}")}
-            .onErrorResume { e -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("오류코드: $e")}
+            .onErrorResume { e ->
+                logger.error(e.stackTraceToString())
+                ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Please contact Genome")
+            }
     }
 
     private fun updateRequests(request: ServerRequest) : Mono<ServerResponse> {
@@ -46,7 +52,10 @@ class RequestRouter(
             .flatMap { requests -> requestHandler.updateRequests(requests.toList()) }
             .then(ServerResponse.ok().build())
             .onErrorResume(AuthenticationNotFoundException::class.java) { e -> ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("${e.message}")}
-            .onErrorResume { e -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("오류코드: $e")}
+            .onErrorResume { e ->
+                logger.error(e.stackTraceToString())
+                ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Please contact Genome")
+            }
     }
 
     private fun orderInfo(request: ServerRequest): Mono<ServerResponse> {
@@ -57,5 +66,9 @@ class RequestRouter(
             .flatMap { ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(Mono.just(it), RequestDTO::class.java) }
             .onErrorResume(AuthenticationNotFoundException::class.java) { e -> ServerResponse.status(HttpStatus.UNAUTHORIZED).bodyValue("${e.message}")}
             .onErrorResume(OrderNotFoundException::class.java) { e -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue("${e.message}") }
+            .onErrorResume { e ->
+                logger.error(e.stackTraceToString())
+                ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Please contact Genome")
+            }
     }
 }
