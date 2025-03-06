3a5a1d7ee5066dbccb99674fa9e2de4023413e6d
xodyd <xodyd@greencross.com>
2025-03-06 16:11:06 +0900

    catalog-service: sort_extension 추가

--- catalog-service/src/main/kotlin/com/gcgenome/rms/dao/ExtensionDao.kt
+++ catalog-service/src/main/kotlin/com/gcgenome/rms/dao/ExtensionDao.kt
@@ -5,19 +5,20 @@ import com.gcgenome.rms.tables.references.EXTENSION
 import com.gcgenome.rms.tables.references.SERVICE
 import com.gcgenome.rms.tables.references.SERVICE_EXTENSION
 import org.jooq.DSLContext
-import org.jooq.impl.DSL
+import org.jooq.impl.DSL.*
 import reactor.core.publisher.Flux
 
 interface ExtensionDao {
     fun DSLContext.selectExtensionByService(serviceId: String): Flux<ServiceExtensionDTO> {
         return Flux.from(
             select(
-                DSL.jsonObject(
-                    DSL.key("id").value(EXTENSION.ID),
-                    DSL.key("name").value(EXTENSION.NAME),
-                    DSL.key("regex").value(EXTENSION.REGEX),
-                    DSL.key("type").value(EXTENSION.TYPE),
-                    DSL.key("required").value(SERVICE_EXTENSION.REQUIRED)
+                jsonObject(
+                    key("id").value(EXTENSION.ID),
+                    key("name").value(EXTENSION.NAME),
+                    key("regex").value(EXTENSION.REGEX),
+                    key("type").value(EXTENSION.TYPE),
+                    key("required").value(SERVICE_EXTENSION.REQUIRED),
+                    key("sort_extension").value(SERVICE_EXTENSION.SORT_EXTENSION)
                 )).from(EXTENSION)
                 .join(SERVICE_EXTENSION).on(EXTENSION.ID.eq(SERVICE_EXTENSION.EXTENSION_ID))
                 .join(SERVICE).on(SERVICE_EXTENSION.SERVICE_ID.eq(SERVICE.ID))
--- catalog-service/src/main/kotlin/com/gcgenome/rms/data/ServiceExtensionDTO.kt
+++ catalog-service/src/main/kotlin/com/gcgenome/rms/data/ServiceExtensionDTO.kt
@@ -1,9 +1,13 @@
 package com.gcgenome.rms.data
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class ServiceExtensionDTO(
     val id: String?,
     val name: String?,
     val regex: String?,
     val required: Boolean?,
-    val type: String?
+    val type: String?,
+    @JsonProperty("sort_extension")
+    val sortExtension: Int?
 )
