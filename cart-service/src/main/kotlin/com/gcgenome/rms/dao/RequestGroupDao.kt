1fb811e375726e021bddbbf31585686474ae5ec2
xodyd <xodyd@greencross.com>
2025-03-18 10:33:55 +0900

    cart-service: request_group 삭제 버그 수정

--- cart-service/src/main/kotlin/com/gcgenome/rms/dao/RequestGroupDao.kt
+++ cart-service/src/main/kotlin/com/gcgenome/rms/dao/RequestGroupDao.kt
@@ -1,10 +1,8 @@
 package com.gcgenome.rms.dao
 
-import com.gcgenome.rms.data.PatientDTO
 import com.gcgenome.rms.data.RequestGroupDTO
-import com.gcgenome.rms.tables.references.PATIENT
+import com.gcgenome.rms.tables.references.REQUEST
 import com.gcgenome.rms.tables.references.REQUEST_GROUP
-import com.gcgenome.rms.tables.references.SAMPLE
 import org.jooq.DSLContext
 import reactor.core.publisher.Mono
 import java.util.*
@@ -12,7 +10,14 @@ import java.util.*
 interface RequestGroupDao {
     fun DSLContext.deleteRequestGroup(requestGroupId: UUID): Mono<RequestGroupDTO> {
         return Mono.from(
-            deleteFrom(REQUEST_GROUP).where(REQUEST_GROUP.ID.eq(requestGroupId)).returning()
+            deleteFrom(REQUEST_GROUP)
+                .where(REQUEST_GROUP.ID.eq(requestGroupId))
+                .andNotExists(
+                    selectOne()
+                        .from(REQUEST)
+                        .where(REQUEST.REQUEST_GROUP_ID.eq(requestGroupId))
+                )
+                .returning()
         ).map { it.into(RequestGroupDTO::class.java) }
     }
 }
