ab14c61b2faf5d7ff086253f0b3ccbdbe4647e3c
xodyd <xodyd@greencross.com>
2025-03-31 09:22:49 +0900

    client: env 수정

--- rms-client/rms/next.config.mjs
+++ rms-client/rms/next.config.mjs
@@ -2,50 +2,51 @@
 const nextConfig = {
     reactStrictMode: false,
     async rewrites() {
+        const baseUrl = process.env.BASE_URL
         return [
             {
                 source: '/w-api/login-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/login-service/:path*`,
+                destination: `${baseUrl}/w-api/login-service/:path*`,
             },
             {
                 source: '/w-api/management-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/management-service/:path*`,
+                destination: `${baseUrl}/w-api/management-service/:path*`,
             },
             {
                 source: '/w-api/organization-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/organization-service/:path*`,
+                destination: `${baseUrl}/w-api/organization-service/:path*`,
             },
             {
                 source: '/w-api/order-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/order-service/:path*`,
+                destination: `${baseUrl}/w-api/order-service/:path*`,
             },
             {
                 source: '/w-api/dashboard-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/dashboard-service/:path*`,
+                destination: `${baseUrl}/w-api/dashboard-service/:path*`,
             },
             {
                 source: '/w-api/cart-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/cart-service/:path*`,
+                destination: `${baseUrl}/w-api/cart-service/:path*`,
             },
             {
                 source: '/w-api/post-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/post-service/:path*`,
+                destination: `${baseUrl}/w-api/post-service/:path*`,
             },
             {
                 source: '/w-api/profile-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/profile-service/:path*`,
+                destination: `${baseUrl}/w-api/profile-service/:path*`,
             },
             {
                 source: '/w-api/catalog-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/catalog-service/:path*`,
+                destination: `${baseUrl}/w-api/catalog-service/:path*`,
             },
             {
                 source: '/w-api/result-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/result-service/:path*`,
+                destination: `${baseUrl}/w-api/result-service/:path*`,
             },
             {
                 source: '/w-api/home-service/:path*',
-                destination: `https://rms-test.gcgenome.com/w-api/home-service/:path*`,
+                destination: `${baseUrl}/w-api/home-service/:path*`,
             }
         ];
     },
